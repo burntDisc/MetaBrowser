6f222ea6a02138addc9d0ca115cba1041c5c251f
gpucigarette <ellistonfranks@gmail.com>
2023-05-02 12:21:33 -0700

    Fixing bugs in "triangle filling curve"

--- SpatialIndexer.cpp
+++ SpatialIndexer.cpp
@@ -1,35 +1,38 @@
 #include "SpatialIndexer.h"
 #include <cmath>
-
+//https://www.youtube.com/watch?v=pw_50szQfA0
 int SpatialIndexer::GetIndex(int level, int index, int baseLength, int maxIndex)
 {
-	int levelsFromMid = (baseLength / 2) - level;
+	int levelsFromMid = (baseLength / 2) - level - 1;
 	if (maxIndex == 0)
 	{
 		return 0;
 	}
-	else if (level >= baseLength / 2)
+	else if (baseLength / 2 <= level)
 	{
+		//top flipped horizontal
 		int adjustedLevel = level - baseLength / 2;
 		int adjustedIndex = (adjustedLevel * 2) - index;
-		return 3 * maxIndex / 4 + GetIndex(adjustedLevel, adjustedIndex, baseLength / 2, maxIndex / 4);
+		return 3 * (maxIndex + 1) / 4 + GetIndex(adjustedLevel, adjustedIndex, baseLength / 2, maxIndex / 4);
 	}
 	else if (index < levelsFromMid * 2 + 1)
-	{
+	{ // left
 		return GetIndex(level, index, baseLength / 2, maxIndex / 4);
 	}
-	else if (index < baseLength / 2)
+	else if (index < baseLength)
 	{
-		return maxIndex / 4 + GetIndex(baseLength/2-level, index, baseLength / 2, maxIndex / 4);
+		//middle flipped vertical
+		return (maxIndex + 1) / 4 + GetIndex(baseLength/2-level, index, baseLength / 2, maxIndex / 4);
 	}
 	else
-	{
-		return maxIndex / 2 + GetIndex(level, index - baseLength / 2, baseLength / 2, maxIndex / 4);
+	{ // right
+		return (maxIndex + 1) / 2 + GetIndex(level, index - baseLength, baseLength / 2, maxIndex / 4);
 	}
 }
  
 int SpatialIndexer::IndexFromPosition(int level, int index, int baseLength, int iterations)
 {
-	int maxIndex = pow(4, iterations);
-	return GetIndex(level, index, baseLength, maxIndex);
+	int maxIndex = pow(4, iterations) - 1;
+	int spatialIndex =  GetIndex(level, index, baseLength, maxIndex);
+	return spatialIndex;
 }
